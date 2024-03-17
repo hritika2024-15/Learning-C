3b85ed591e0028d8a6a758c3f85c3b7a345bc6b0
hritika2024-15 <hritikaprasad15@gmail.com>
2024-03-17 09:25:08 +0530

    performing division and subtraction

---
+++ codechef/course/LBCL04_problems_CLB018.c
@@ -0,0 +1,21 @@
+/*Division
+We have already done addition and multiplication inside print. Let's do subtraction and division as well once.
+Task
+Write a program which does the following
+
+Print the result of subtraction of 9 and 2
+Print the result of division of 30 by 5
+Output both results on separate lines*/
+#include <stdio.h>
+
+int main() {
+	// Print the result of 9 - 2
+	printf("%d\n",9-2);
+	
+	
+	// Print the result of 30 / 5
+	printf("%d",30/5);
+	return 0;
+	
+	
+}
