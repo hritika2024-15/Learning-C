6a84ce8fa0177a6385efc0052945547f0957af80
hritika2024-15 <hritikaprasad15@gmail.com>
2024-03-17 09:55:36 +0530

    learning about strings

---
+++ codechef/course/LBCL08_problems_CLB038.c
@@ -0,0 +1,17 @@
+/*Creating a String
+Strings are used for storing text.
+
+A string variable contains a collection of characters surrounded by double quotes. Strings are implemented as character arrays in C.
+Task
+Write a program which does the following
+
+Create a string named fruit and assign the value "Apple" to it.
+Output fruit to the console.*/
+#include <stdio.h>
+
+int main() {
+
+    char fruit[] = "Apple";
+    printf("%s", fruit);
+
+}
