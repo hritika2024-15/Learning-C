6f48f4daab1849ccaa2f654c11a0633d842b5282
hritika2024-15 <hritikaprasad15@gmail.com>
2024-03-17 12:21:02 +0530

    learning about arrays of integer type

---
+++ codechef/course/LBCL18_problems_CLB086.c
@@ -0,0 +1,15 @@
+/*Creating Arrays
+Task
+Write a program which does the following
+
+Create an Array of the first 5 positive integers
+Once the array is define - output "Done" to the console.*/
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int numbers[5]={1,2,3,4,5};
+	printf("Done");
+	
+
+}
