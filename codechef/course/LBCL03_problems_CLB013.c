e514fc6246515a036edf9e49084910b343b5c33c
hritika2024-15 <hritikaprasad15@gmail.com>
2024-03-17 09:06:42 +0530

    printing with a space

---
+++ codechef/course/LBCL03_problems_CLB013.c
@@ -0,0 +1,11 @@
+/*Inserting space between outputs
+You can also have multiple outputs together in a single "printf()" function.
+Task
+Write a program to output 7 and 3 on a single line, space separated, using a single printf statement.*/
+#include <stdio.h>
+
+int main() {
+    // Add two %d to print two numbers
+    printf("%d %d", 7, 3);
+	
+}
