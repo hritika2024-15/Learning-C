ca731a0efe775003ffca3cdd0d6a9c73e50a9cdb
hritika2024-15 <hritikaprasad15@gmail.com>
2024-03-17 11:10:28 +0530

    using if without an else statement

---
+++ codechef/course/LBCL13_problems_CLB066.c
@@ -0,0 +1,20 @@
+/*If Statement
+The else statement is optional.
+Task
+Write a program which does the following
+
+Take input for two integer variables a & b
+Output "Coding is Fun" to the console if a is greater than b.*/
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int a;
+	int b;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	if(a>b){
+	    printf("Coding is Fun");
+	}
+    
+}
