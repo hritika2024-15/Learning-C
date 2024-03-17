2545170ee55d17fdda88336b37a93ef3c7b24b8f
hritika2024-15 <hritikaprasad15@gmail.com>
2024-03-17 14:12:52 +0530

    learning test cases

---
+++ codechef/course/LBCL24_problems_GSCP09A.c
@@ -0,0 +1,23 @@
+/*What are test cases
+Task
+Lets solve a simple problem.
+Write a program in the IDE which does the following
+
+Accepts 5 inputs given on 5 separate lines. Each input is an integer N
+For each test cases, prints out the integer N to console on a separate line (our Input mirror problem)*/
+#include <stdio.h>
+
+int main() {
+    int A, B, C, D, E;
+    scanf("%d \n", &A );
+    scanf("%d \n", &B );
+    scanf("%d \n", &C);
+    scanf("%d \n", &D );
+    scanf("%d \n", &E );
+    printf("%d \n", A );  
+    printf("%d \n", B );  
+    printf("%d \n", C );  
+    printf("%d \n", D );  
+    printf("%d \n", E );  
+    
+}
