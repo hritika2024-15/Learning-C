7d6732a63f73787b930307e01b902ba0530ba4a8
hritika2024-15 <hritikaprasad15@gmail.com>
2024-03-17 09:40:24 +0530

    learning about how to declare a variable

---
+++ codechef/course/LBCL05_problems_CLB023.c
@@ -0,0 +1,18 @@
+/*Declaring a variable
+When we declare a variable, we should also specify what type of variable it is. This is known as data type, basically meaning what type of data we are going to store in a variable.
+Task
+Write a program which does the following
+
+Create an integer variable named number
+Assign the value 19 to number
+Output the value of number.*/
+#include <stdio.h>
+
+int main() {
+    // Declare a variable
+    int number = 19;
+    
+    // Print the variable
+    printf("%d", number);
+  
+}
