a6ef3539a08911bbc88c62c67f33dfc0d78503e5
hritika2024-15 <hritikaprasad15@gmail.com>
2024-03-17 12:35:41 +0530

    printing specific index of an array

---
+++ codechef/course/LBCL18_problems_CLB087.c
@@ -0,0 +1,19 @@
+/*Index & printing specific elements of an Array
+Task
+Write a program which does the following
+
+Output the 3rd element from the given array to the console.
+Task
+Write a program which does the following
+
+Output the 3rd element from the given array to the console.*/
+#include <stdio.h>
+
+int main() {
+    
+    int num[ 5 ] = {1, 2, 3, 4, 5 };
+    // your code goes here
+    printf("%d",num[2]);
+    
+
+}
