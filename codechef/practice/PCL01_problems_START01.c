54b236444952267a0ad372894eaa33ddff6db6b3
hritika2024-15 <hritikaprasad15@gmail.com>
2024-03-17 19:10:03 +0530

    practice problem 1: Number Mirror

---
+++ codechef/practice/PCL01_problems_START01.c
@@ -0,0 +1,19 @@
+/*Number Mirror
+Write a program that takes a number 
+N as the input, and prints it to the output.
+
+Input Format
+The only line of input contains a single integer.
+
+Output Format
+Output the answer in a single line.*/
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int x;
+	scanf("%d",&x);
+	printf("%d",x);
+	
+
+}
