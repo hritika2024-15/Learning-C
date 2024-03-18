a33d50a61f96a50fb9a353d5783cc3a3e0b1c8c4
hritika2024-15 <hritikaprasad15@gmail.com>
2024-03-18 21:29:22 +0530

    printing hello world alomg with a given user input

---
+++ hackerrank/challenges_hello-world-c_problem.c
@@ -0,0 +1,17 @@
+/*Task
+
+This challenge requires you to print Hello, World! on a single line, and then print the already provided input string to stdout.
+ If you are not familiar with C, you may want to read about the printf() command.*/
+ #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+char p[100];
+scanf("%[^\n]",p);
+printf("Hello, World!\n");
+printf("%s",p);
+return 0;
+}
